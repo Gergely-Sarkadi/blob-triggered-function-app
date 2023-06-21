4c4db6e3c25ff2c20637f0bbf35b17a07e862452
Gergely Sarkadi <gergely.sarkadi@eongroup.co>
2023-06-21 13:00:25 +0200

    Refactor function app code

--- QueueTrigger1/__init__.py
+++ QueueTrigger1/__init__.py
@@ -5,27 +5,34 @@ import vt
 
 import azure.functions as func
 from azure.identity import ManagedIdentityCredential
-from azure.storage.blob import BlobServiceClient, BlobClient, ContainerClient
+from azure.storage.blob import BlobServiceClient
 from urllib.parse import urlparse
 
 
 def get_blob_service_client_token_credential(account_url):
+    '''Get a token for an account with managed identity.'''
     credential = ManagedIdentityCredential()
     return BlobServiceClient(account_url, credential=credential)
 
 def parse_url(blob_url):
+    '''Parse out and return the container name and the file name from a blob url.'''
     parsed_url = urlparse(blob_url)
     container_name = parsed_url.path.split('/')[1]
     blob_name = '/'.join(parsed_url.path.split('/')[2:])
     return (container_name, blob_name)
 
 def parse_filename(filename):
+    '''Parse out and return the name and extension of a file. Like: 'hello.txt' -> (hello, txt)'''
     split_name = filename.split('.')
     name = '.'.join(split_name[:-1])
     extension = split_name[-1] if len(split_name) > 1 else ''
     return (name, extension)
 
 def check_file_hash_for_viruses(md5_hash):
+    '''
+    end the checksum of the blob to the VirusTotal API for virus checking.
+    Set the API key as an environmental variable of the Function App.
+    '''
     client = vt.Client(os.environ['VIRUSTOTAL_API_KEY'])
     try:
         file = client.get_object(f'/files/{md5_hash}')
@@ -36,14 +43,10 @@ def check_file_hash_for_viruses(md5_hash):
         return True
 
 def main(msg: func.QueueMessage) -> None:
-    logging.info(f'Function App starts')
-
     # Download and process blob
     source_blob_url = msg.get_json()['data']['blobUrl']
-    # source_account_url = "https://sablobtriggertestsource.blob.core.windows.net"
-    # source_blob_service_client = get_blob_service_client_token_credential(source_account_url)
-    source_connection_string = os.environ['SOURCE_STORAGE_ACCOUNT_CONN_STRING']
-    source_blob_service_client = BlobServiceClient.from_connection_string(source_connection_string)
+    # source_blob_service_client = get_blob_service_client_token_credential("https://<your source service accounts name>.blob.core.windows.net")
+    source_blob_service_client = BlobServiceClient.from_connection_string(os.environ['SOURCE_STORAGE_ACCOUNT_CONN_STRING'])
 
     container_name, source_blob_name = parse_url(source_blob_url)
     source_blob_client = source_blob_service_client.get_blob_client(container=container_name, blob=source_blob_name)
@@ -56,16 +59,15 @@ def main(msg: func.QueueMessage) -> None:
         logging.info(f'The uploaded file is safe. Upload it to the second Storage Account.')
 
         # Upload blob
-        # target_account_url = "https://sablobtriggertesttarget.blob.core.windows.net"
-        # target_blob_service_client = get_blob_service_client_token_credential(target_account_url)
-        target_connection_string = os.environ['TARGET_STORAGE_ACCOUNT_CONN_STRING']
-        target_blob_service_client = BlobServiceClient.from_connection_string(target_connection_string)
+        # target_blob_service_client = get_blob_service_client_token_credential("https://<your target service accounts name>.blob.core.windows.net")
+        target_blob_service_client = BlobServiceClient.from_connection_string(os.environ['TARGET_STORAGE_ACCOUNT_CONN_STRING'])
 
-        # Check if a container exists on the target as the same name with the source container, create it if necessary
+        # Check if a container exists on the target with the same name as the source container, create it if necessary
         target_container_client = target_blob_service_client.get_container_client(container_name)
         if not target_container_client.exists():
             target_container_client.create_container()
 
+        # Put the checksum of the blob in its name and upload.
         blob_name, extension = parse_filename(source_blob_name)
         target_blob_name = f'{blob_name}.{checksum}.{extension}'
         logging.info(f'Target blob name: {target_blob_name}')
@@ -77,5 +79,5 @@ def main(msg: func.QueueMessage) -> None:
 
         # TODO necessary steps if a file is infected (send an alarm, block the user, put the file in quaranteen, etc.)
 
-    # Delete original blob
+    # Delete the original blob
     source_blob_client.delete_blob()
